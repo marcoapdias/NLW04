549c427d2545728e60428d968ab6164b33f2ce0d
MarcoAPDias <marcoapdias@gmail.com>
2021-02-24 09:32:49 -0300

    NLW04 - Day 3 - Survey Repository

--- aulas/api/src/controllers/UserController.ts
+++ aulas/api/src/controllers/UserController.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
-
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         // Select * from Users where email = "email"
         const userAlreadyExists = await usersRepository.findOne({
@@ -20,7 +19,6 @@ class UserController {
             });
         }
 
-
         const user = usersRepository.create({
             name, email
         });
@@ -31,4 +29,4 @@ class UserController {
     }
 }
 
-export { UserController }
+export { UserController };
